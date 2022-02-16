ad753ada294d30e355c1fc37f4c9ab62eb3c6c29
Juan Selva <juanorlandoselvamadrigal@gmail.com>
2022-02-16 02:59:37 -0600

    Initial Commit

---
+++ src/main/java/com/jselva/nisum/evaluationtest/EvaluationtestApplication.java
@@ -0,0 +1,13 @@
+package com.jselva.nisum.evaluationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jselva/nisum/evaluationtest/EvaluationtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jselva.nisum.evaluationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
